4b93349f0074ec713df89dd59fd131455f8df625
test <test@KAB42-3>
2022-10-08 11:54:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp14;
+
+Reader reader=new Reader();
+void GetMenu()
+{
+    Console.WriteLine($"1) Взять книги");
+}
+void Menu()
+{
+    GetMenu();
+    string a= Console.ReadLine();
+    int menu= int.Parse(a);
+    if (menu == 1)
+    {
+        Console.Write($"Имя:");
+        string name= Console.ReadLine();
+        Console.Write($"фамилия :");
+        string surname = Console.ReadLine();
+        Console.Write($"Отчество:");
+        string patronymic = Console.ReadLine();
+        Console.Write($"количество взятых книг:");
+        string book = Console.ReadLine();
+        int nbook= int.Parse(a);
+        Console.Write($"Номер телефона: ");
+        string number= Console.ReadLine();
+        reader.TakeBook(name,surname,patronymic,nbook,number);
+        Reader reader2= new Reader(name, surname, patronymic, nbook, number);
+    }
+}
+Menu();
---
+++ ConsoleApp14/Reader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Reader
+    {
+        private string _name;//имя
+        private string _surname;//фамилия
+        private string _patronymic;//отчество
+        private string _faculty;//факультет
+        private string _dateofbirth;//дата рождения
+        private string _number;//телефон
+        private List<Reader> _info= new List<Reader>();
+        private int _book;
+        public Reader(string name, string surname, string patronymic, int book, string number)
+        {
+            _name = name;
+            _surname = surname;
+            _patronymic = patronymic;
+            _book = book;
+            _number = number;
+        }
+        public Reader()
+        {
+
+        }
+        public void TakeBook()//взять книгу
+        {
+           if (_book == )
+           {
+                Console.WriteLine($"можете идти");
+           }
+           else if (_book == 0)
+            Console.WriteLine($"");
+            
+        }
+        public void TakeBook( string name, string surname,string patronymic,int book, string number)
+        {
+            Console.WriteLine($"{name}.{surname}.{patronymic}. взял: {book} книг");
+            _book=book;
+        }
+        public void Get(Reader reader1)
+        {
+            _info.Add(reader1);
+        }
+        
+        public void ReturnBook()//вернуть книгу
+        {
+
+        }
+
+
+    }
+}
