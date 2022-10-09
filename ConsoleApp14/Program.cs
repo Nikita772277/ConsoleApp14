1e1790c03da6f6f68803d85b39a0ae0b3e122bf8
Nikita77227 <Nikita77227@DESKTOP-029TR2H>
2022-10-09 18:09:47 +0300

    12

--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -1,30 +1,53 @@
 ﻿using ConsoleApp14;
+using System.ComponentModel;
+using System.Xml.Linq;
 
-Reader reader=new Reader();
+
 void GetMenu()
 {
     Console.WriteLine($"1) Взять книги");
+    Console.WriteLine($"2) Вернуть книги");
+    Console.WriteLine($"3) Информация о взявших книги");
 }
 void Menu()
-{
-    GetMenu();
-    string a= Console.ReadLine();
-    int menu= int.Parse(a);
-    if (menu == 1)
+{  
+    Reader reader = new Reader();
+    while (true)
     {
-        Console.Write($"Имя:");
-        string name= Console.ReadLine();
-        Console.Write($"фамилия :");
-        string surname = Console.ReadLine();
-        Console.Write($"Отчество:");
-        string patronymic = Console.ReadLine();
-        Console.Write($"количество взятых книг:");
-        string book = Console.ReadLine();
-        int nbook= int.Parse(a);
-        Console.Write($"Номер телефона: ");
-        string number= Console.ReadLine();
-        reader.TakeBook(name,surname,patronymic,nbook,number);
-        Reader reader2= new Reader(name, surname, patronymic, nbook, number);
+        GetMenu();
+        
+        string a = Console.ReadLine();
+        int menu = int.Parse(a);
+        if (menu == 1)
+        {
+            Console.Write($"Имя: ");
+            string name = Console.ReadLine();
+            Console.Write($"фамилия: ");
+            string surname = Console.ReadLine();
+            Console.Write($"Отчество: ");
+            string patronymic = Console.ReadLine();
+            Console.Write($"количество взятых книг: ");
+            string book = Console.ReadLine();
+            int nbook = int.Parse(book);
+            Console.Write($"Номер телефона: ");
+            string number = Console.ReadLine();
+            Reader reader2 = new Reader(name, surname, patronymic, nbook, number);
+            Console.WriteLine();
+            reader2.Get(reader2);
+            reader.TakeBook(name, surname, patronymic, nbook, number);
+        }
+        else if (menu == 2)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Введите фамилию");
+            string surname= Console.ReadLine();
+            reader.ReturnBook(surname);
+        }
+        else if (menu == 3)
+        {
+            
+            reader.GetInfo();
+        }
     }
 }
 Menu();
--- ConsoleApp14/Reader.cs
+++ ConsoleApp14/Reader.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ConsoleApp14
 {
@@ -14,8 +16,12 @@ namespace ConsoleApp14
         private string _faculty;//факультет
         private string _dateofbirth;//дата рождения
         private string _number;//телефон
-        private List<Reader> _info= new List<Reader>();
+        List<Reader> info= new List<Reader>();
         private int _book;
+        public Reader()
+        {
+
+        }
         public Reader(string name, string surname, string patronymic, int book, string number)
         {
             _name = name;
@@ -24,33 +30,57 @@ namespace ConsoleApp14
             _book = book;
             _number = number;
         }
-        public Reader()
-        {
-
-        }
         public void TakeBook()//взять книгу
         {
-           if (_book == )
-           {
-                Console.WriteLine($"можете идти");
-           }
-           else if (_book == 0)
-            Console.WriteLine($"");
-            
+            //if (_book == )
+            //{
+            //     Console.WriteLine($"можете идти");
+            //}
+            //else if (_book == 0)
+            // Console.WriteLine($"");
+
         }
-        public void TakeBook( string name, string surname,string patronymic,int book, string number)
+        public void TakeBook(string name, string surname, string patronymic, int book, string number)
         {
             Console.WriteLine($"{name}.{surname}.{patronymic}. взял: {book} книг");
-            _book=book;
+            _book = book;
         }
         public void Get(Reader reader1)
         {
-            _info.Add(reader1);
+            info.Add(reader1);
         }
-        
-        public void ReturnBook()//вернуть книгу
-        {
 
+        public void ReturnBook(string surname)//вернуть книгу
+        {
+            foreach (Reader reader in info)
+            {
+                if (_surname == surname)
+                {
+                    info.Remove(reader);
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"нет такой фамилии");
+                    Console.WriteLine();
+                }
+            }
+        }
+        public void GetInfo()
+        {
+            foreach (Reader read in info)
+            {
+                Console.WriteLine($"{read}");
+                //Console.WriteLine();
+                //Console.WriteLine($"Имя: {_name}");
+                //Console.WriteLine($"Фамилия: {_surname}");
+                //Console.WriteLine($"Отчества {_patronymic}");
+                //Console.WriteLine($"Количество взятых книг: {_book}");
+                //Console.WriteLine($"Номер телефона: {_number}");
+                //Console.WriteLine();
+            }
+                
         }
 
 
